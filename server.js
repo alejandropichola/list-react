0a8a71c16f1f98e18c0f293851c28414c829313c
Alejandro Pichola <g4alejandro29@gmail.com>
2021-11-12 20:31:47 -0600

    change server

--- server.js
+++ server.js
@@ -1,15 +1,7 @@
 const express = require("express");
-const path = require("path");
-const port = process.env.PORT || 8080;
-const app = express();
-app.use(express.static(__dirname));
-app.use(express.static(path.join(__dirname, "build")));
-app.get("/ping", function (req, res) {
-  return res.send("pong");
-});
+const app = express()
+const PORT = process.env.PORT || 3000
 
-app.get("/*", function (req, res) {
-  res.sendFile(path.join(__dirname, "build", "index.html"));
-});
+app.use(express.static('build'))
 
-app.listen(port);
+app.listen(PORT, () => console.log(`Port ${PORT}`));
